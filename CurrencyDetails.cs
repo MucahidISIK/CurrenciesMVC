4afe5bbacba8f3fc24ed7f9e30c9cc45aa043f33
Mücahid IŞIK <56644702+MucahidISIK@users.noreply.github.com>
2022-08-25 14:31:33 +0300

    Add files via upload

---
+++ CurrencyDetails.cs
@@ -0,0 +1,25 @@
+﻿namespace CurrenciesMVC
+{
+    public class CurrencyDetails
+    {
+        private string Currency;
+        private double Buying;
+        private double Selling;
+
+        public string Currency_
+        {
+            get { return Currency; }
+            set { Currency = value; }
+        }
+        public double Buying_
+        {
+            get { return Buying; }
+            set { Buying = value; }
+        }
+        public double Selling_
+        {
+            get { return Selling; }
+            set { Selling = value; }
+        }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CurrenciesMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CurrenciesMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using (CurrencyService.CurrencyServiceClient serviceClient_ = new CurrencyService.CurrencyServiceClient())
+            {
+
+                var apiData = serviceClient_.getCurrencyInfoAsync().Result;
+                List<CurrencyDetails> model = new List<CurrencyDetails>();
+                for(int i=0; i<apiData.Length; i++)
+                {
+                    CurrencyDetails tempData = new CurrencyDetails();
+                    tempData.Currency_ = apiData[i].Currency_;
+                    tempData.Buying_ = apiData[i].Buying_;
+                    tempData.Selling_ = apiData[i].Selling_;
+                    model.Add(tempData);
+                }
+                return View(model);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
